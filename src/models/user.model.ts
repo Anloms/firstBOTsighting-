9e551f2633c6f9da402c42a9b2eac8d7f179c660
Oweley <aleksandra.smolna@gmail.com>
2024-12-06 19:56:30 +0000

    prisma addWalletToTrack & walletAlreadyTracked function added

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,18 +1,47 @@
 
 import prisma from '../../prisma/prismaClient.js';
 
-export async function createUser(userId: string) {
+export async function createUser(telegramId: string) {
    await prisma.user.create({
     data: {
-      telegramId: userId,
+      telegramId: telegramId,
     },
   });
   return
 }
 
 export async function userExists(telegramId: string) {
-   await prisma.user.findUnique({
+   const value = await prisma.user.findUnique({
     where: { telegramId },
   });
-  return
+  return value
+
+}
+export async function walletAlreadyTracked(accountAddress:string, telegramId:string){
+  const value = await prisma.wallet.findUnique({
+    where: {
+      id: telegramId,
+      accountAddress: accountAddress
+    }
+  })
+  return value
+}
+export async function addWalletToTrack(accountAddress: string, telegramId: string){
+  const alreadyTracked = await walletAlreadyTracked(accountAddress, telegramId)
+  if(alreadyTracked) {
+    return "already tracked"
+  } else {
+    const user = await prisma.user.findUnique({where: {
+      id: telegramId,
+    }})
+    if(!user) {
+      throw new Error("User not found")
+    }
+    await prisma.wallet.create({
+      data:{
+        accountAddress: accountAddress,
+        userId: user.id
+      }
+    })
+  }
 }
